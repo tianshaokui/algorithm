45bc042365f7067b108b48fb5875e3ee1979b15b
tianshaokui <Kesker@163.com>
2016-06-13 11:22:43 +0800

    modified stl

--- stl_iostream.cpp
+++ stl_iostream.cpp
@@ -11,9 +11,8 @@ int main()
 	copy(istream_iterator<double>(cin), istream_iterator<double>(), back_inserter(dList));   // back_inserter_iterator  inserter_iterator
 	copy(dList.begin(), dList.end(),ostream_iterator<double>(cout, ", "));
 	cout<<"\b\b  "<<endl;
-	//for_each(dList.begin(), dList.end(), [](double a){ return a+10;});
-	for(list<double>::iterator i=dList.begin(); i!=dList.end(); i++)
-		*i += 10;
-	int a = [](){return 3;}();
+	for_each(dList.begin(), dList.end(), [](double &a){ a+=10;});
+	copy(dList.begin(), dList.end(),ostream_iterator<double>(cout, ", "));
+	cout<<"\b\b  "<<endl;
 	return 0;
 }
